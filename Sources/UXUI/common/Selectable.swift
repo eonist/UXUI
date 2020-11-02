49fd7ceac44c36a544cd5e8c731e73fa5590fd64
eonist <30n1st@gmail.com>
2020-11-02 13:02:41 +0100

    Update Selectable.swift

--- Sources/UXUI/common/Selectable.swift
+++ Sources/UXUI/common/Selectable.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 public protocol Selectable: AnyObject {
-   public var selected: Bool { get set } // { get { return getSelected() } set { setSelected(newValue) } }
+   var selected: Bool { get set } // { get { return getSelected() } set { setSelected(newValue) } }
 }
