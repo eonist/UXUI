383ec76b4c3134669032e82fe644b22425812d76
eonist <30n1st@gmail.com>
2020-11-03 15:57:34 +0100

    Bump deps

--- Sources/UXUI/Label/UXLabel+Event.swift
+++ Sources/UXUI/Label/UXLabel+Event.swift
@@ -3,6 +3,9 @@ import Cocoa
  * Event
  */
 extension UXLabel /*NSTextFieldDelegate*/ {
+   /**
+    * - Note: there is also: textShouldBeginEditing, textShouldEndEditing, textDidBeginEditing, textDidEndEditing
+    */
    override open func textDidChange(_ notification: Notification) {
       super.textDidChange(notification)
       onTextFieldChange() // Perform search action on every Text change
