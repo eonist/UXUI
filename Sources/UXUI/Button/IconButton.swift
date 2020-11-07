7b90294e321c582eb9f8920f523de760b0a3bb37
eonist <30n1st@gmail.com>
2020-11-07 12:59:55 +0100

    Make IconButton variables public

--- Sources/UXUI/Button/IconButton.swift
+++ Sources/UXUI/Button/IconButton.swift
@@ -5,8 +5,8 @@ import Spatial
  * - Fixme: ⚠️️ Test this in example, and implement darkmode for images similar to ios
  */
 open class IconButton: UXButton {
-   let iconURL: URL
-   lazy var imageView: NSImageView = createImageView()
+   public let iconURL: URL
+   public lazy var imageView: NSImageView = createImageView()
    public init(style: UXButton.Style = .default, iconURL: URL, frame: CGRect = .zero) {
       self.iconURL = iconURL
       super.init(style: style, frame: frame)
@@ -20,7 +20,7 @@ extension IconButton {
    /**
     * Create imageView
     */
-   func createImageView() -> NSImageView {
+   @objc open func createImageView() -> NSImageView {
       with(.init()) {
          $0.image = NSImage(contentsOf: iconURL)
          addSubview($0)
