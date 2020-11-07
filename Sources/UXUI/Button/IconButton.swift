c193b161363893c0f088543b764d06c3045d459f
eonist <30n1st@gmail.com>
2020-11-07 19:22:14 +0100

    Add named asset support to IconButton

--- Sources/UXUI/Button/IconButton.swift
+++ Sources/UXUI/Button/IconButton.swift
@@ -5,10 +5,12 @@ import Spatial
  * - Fixme: ⚠️️ Test this in example, and implement darkmode for images similar to ios
  */
 open class IconButton: UXButton {
-   public let iconURL: URL
+   public let iconURL: String
+   public let isNamed: Bool
    public lazy var imageView: NSImageView = createImageView()
-   public init(style: UXButton.Style = .default, iconURL: URL, frame: CGRect = .zero) {
+   public init(iconURL: String, isNamed: Bool = false, style: UXButton.Style = IconButton.emptyStyle, frame: CGRect = .zero) {
       self.iconURL = iconURL
+      self.isNamed = isNamed
       super.init(style: style, frame: frame)
       _ = imageView
    }
@@ -22,15 +24,17 @@ extension IconButton {
     */
    @objc open func createImageView() -> NSImageView {
       with(.init()) {
-         $0.image = NSImage(contentsOf: iconURL)
+         $0.image = isNamed ? NSImage(contentsOf: URL(fileURLWithPath: iconURL)) : NSImage(named: iconURL)
          addSubview($0)
          $0.anchorAndSize(to: self)
       }
    }
 }
 /**
- * Layout
+ * Style
  */
-//override open func layout() {
-//   super.layout()
-//}
+extension IconButton {
+   public static let emptyStyle: UXButton.Style = {
+      .init(backgroundColor: .clear, borderColor: .clear, borderWidth: 0, isRounded: false)
+   }()
+}
